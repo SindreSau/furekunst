ae7f69233c6c46206bc030dcb680bf6ec5c87e57
Sindre Sauarlia <101213882+SindreSau@users.noreply.github.com>
2024-07-22 18:39:30 +0200

    commit and push before branching out

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                pastel: {
+                    light: '#f4fcff',
+                    DEFAULT: '#FFF',
+                    dark: '#c8f1ff',
+                },
+                primary: {
+                    light: '#454545',
+                    DEFAULT: '#393939',
+                    dark: '##121212',
+                },
+            },
+            fontFamily: {
+                sans: ['Open Sans', 'sans-serif'],
+                kanit: ['Kanit', 'sans-serif'],
+                didot: ['Didot', 'serif'],
+            },
+        },
     },
     plugins: [],
 };
